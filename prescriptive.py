99eafc37f9d08c31a0b074a46165759c99b1e651
sidmehta123 <108069917+sidmehta123@users.noreply.github.com>
2026-06-24 20:16:28 +0530

    prescriptive.py

--- prescriptive.py
+++ prescriptive.py
@@ -1,4 +1,25 @@
 import streamlit as st
 
 def show(df):
+
     st.title("🚀 Prescriptive Analysis")
+
+    st.markdown("Recommended actions based on insights")
+
+    st.success("""
+    Recommendation 1
+    
+    Increase marketing spend on Instagram Ads.
+    """)
+
+    st.info("""
+    Recommendation 2
+    
+    Focus on Sofa and Bed categories.
+    """)
+
+    st.warning("""
+    Recommendation 3
+    
+    Improve delivery speed in Tier 2 cities.
+    """)
