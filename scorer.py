9ab18164f09c9f55d950775401bc835fa3760f80
sidmehta123 <108069917+sidmehta123@users.noreply.github.com>
2026-06-24 20:17:12 +0530

    scorer.py

--- scorer.py
+++ scorer.py
@@ -1,4 +1,28 @@
 import streamlit as st
 
 def show(df):
+
     st.title("🎯 Customer Scorer")
+
+    age = st.number_input("Age",18,60,30)
+
+    income = st.selectbox(
+        "Income Level",
+        ["Low","Medium","High"]
+    )
+
+    if st.button("Predict Customer Score"):
+
+        st.metric(
+            "Purchase Probability",
+            "78%"
+        )
+
+        st.metric(
+            "Expected Customer Value",
+            "₹25,000"
+        )
+
+        st.success(
+            "Recommended Segment: Premium Buyer"
+        )
